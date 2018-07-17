925dd087cdbe9e1c0ddb4bc70f5a683bca38bebc
Craig Anderson <cranderson0110@gmail.com>
2018-07-18 11:21:01 +1200

    Added splosions
    They're cool

---
+++ Assets/Scripts/collisionHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionHandler : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			Instantiate(explosion, other.transform.position, Quaternion.identity);
+			Destroy(other.gameObject);
+		}
+	}
+}
